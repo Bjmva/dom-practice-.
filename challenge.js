0b8613b8156ef928b42e9559001c079a06f79a10
= <=>
2023-10-02 19:50:08 +0100

    added if-else-chained and switch statement

--- challenge.js
+++ challenge.js
@@ -1,16 +1,64 @@
-for (let i = 0; i <20; i++)  
-    console.log ("balqiis")
+for (let i =0; i <30; i++)
+   console.log ("ifrah")
 
 
 
 
 
-    
-let i = 0;
-while (i <20) {
 
-    console.log("jama")
-     i++
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let i = 0; i <10; i++)
+    console.log ("billy")
+
+
+
+
+
+      
+let i = 0;   
+ while (i <5) {
+
+    console.log ("jalo")
+    i++
+ }
+
+
+
+
+
+
+let today = "monday";
+
+if(today == "Monday") {
+   console.log("It is Coding Day")
+} else if (today == "Tuesday") {
+   console.log("It is my off day")
+} else {
+   console.log("ask me personally which day")
 }
 
 
+switch (today) {
+   case "Monday":
+       console.log("It is Coding Day, from switch")
+       break;
+   case "Tuesday":
+       console.log("It is my off day, from switch")
+       break;
+
+   default:
+       console.log("ask me personally which day, from switch")
+       break;
+}
