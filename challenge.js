5afba9a275d4b190963c15d1af51ea172c09e031
= <=>
2023-10-09 20:24:05 +0100

    added function

--- challenge.js
+++ challenge.js
@@ -1,5 +1,17 @@
-for (let i =0; i <30; i++)
-   console.log ("ifrah")
+
+// for (let i = 0; i <20; i++)  
+// console.log ("balqiis")
+
+
+
+
+
+// let i = 0;
+// while (i <20) {
+
+// console.log("jama")
+// i++
+// }
 
 
 
@@ -19,46 +31,128 @@ for (let i =0; i <30; i++)
 
 
 
-for (let i = 0; i <10; i++)
-    console.log ("billy")
+
+
+
+
+//  let today = "monday";
+
+//  if(today == "Monday") {
+//     console.log("It is Coding Day")
+//  } else if (today == "Tuesday") {
+//     console.log("It is my off day")
+//  } else {
+//     console.log("ask me personally which day")
+//  }
+ 
 
 
 
 
 
-      
-let i = 0;   
- while (i <5) {
+//  switch (today) {
+//    case "Monday":
+//        console.log("It is Coding Day, from switch")
+//        break;
+//    case "Tuesday":
+//        console.log("It is my off day, from switch")
+//        break;
 
-    console.log ("jalo")
-    i++
- }
+//    default:
+//        console.log("ask me personally which day, from switch")
+//        break;
+// }
 
 
 
 
 
 
-let today = "monday";
 
-if(today == "Monday") {
-   console.log("It is Coding Day")
-} else if (today == "Tuesday") {
-   console.log("It is my off day")
-} else {
-   console.log("ask me personally which day")
+
+
+
+// // write a for loop that prints your name 20 times
+
+// for(let i = 0; i < 20; i++) {
+//    console.log("Ahmed")
+// }
+
+
+// // write a while that prints your surname 20 times
+// let i = 0;
+// while (i < 20) {
+//    console.log("Dini");
+//    i++;
+// }
+
+
+// // write if/else if, chained code to check what day of the day it is and print something
+
+// let today = "monday";
+
+// if(today == "Monday") {
+//    console.log("It is Coding Day")
+// } else if (today == "Tuesday") {
+//    console.log("It is my off day")
+// } else {
+//    console.log("ask me personally which day")
+// }
+
+// // convert the above if/else to switch
+
+// switch (today) {
+//    case "Monday":
+//        console.log("It is Coding Day, from switch")
+//        break;
+//    case "Tuesday":
+//        console.log("It is my off day, from switch")
+//        break;
+
+//    default:
+//        console.log("ask me personally which day, from switch")
+//        break;
+// }
+
+
+
+
+
+
+
+// let i = 0;
+
+// do {
+//    console.log ("dini from do while");
+//    i++;
+   
+// }
+//  while (i<5)
+
+
+
+
+// function multiplyAndPrint(num1, num2) {
+//    const result = num1 * num2;
+//    console.log("The result of multiplication is: " + result);
+//    return result;
+//  }
+ 
+//  // Example usage:
+//  const number1 = 5;
+//  const number2 = 10;
+//  const multiplicationResult = multiplyAndPrint(number1, number2);
+
+
+
+
+
+
+function multiply (num1, num2) {
+
+   let result = num1 * num2;
+   return result;
 }
-
-
-switch (today) {
-   case "Monday":
-       console.log("It is Coding Day, from switch")
-       break;
-   case "Tuesday":
-       console.log("It is my off day, from switch")
-       break;
-
-   default:
-       console.log("ask me personally which day, from switch")
-       break;
-}
+   
+let sum = multiply (7,8)
+console.log (sum)
---
+++ challenge2.js
@@ -0,0 +1,28 @@
+for (let i =0; i <100; i++)
+    console.log ("billy")
+
+
+    let i =0;
+    while (i <2){
+
+       console.log ("lol")
+
+       i++
+
+
+
+
+    }
+
+     
+    let today = "black";
+    if(today == "black") {
+
+      console.log("red")
+
+      } else if (today == "red" ) {
+
+        console.log("blue")
+      } else {
+        console.log("pink")
+      };
